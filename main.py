9cb7963320878789e57ad8bab0ff10818f4490be
hilalaksu1 <97305435+hilalaksu1@users.noreply.github.com>
2022-01-16 10:59:16 +0300

    format fonction

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+name="çınar"
+surname="turan"
+age=10
+#print("my name is {} {}".format(name, surname))
 
+#print("my name is {1} {0}".format(name, surname))
+#print("my name is {s} {n}".format(n=name, s=surname))
 
+#print("my name is {} {} and I am {} years old".format(name, surname,age))
 
+#result=200/700
+#print("the result is {r:1.4}".format(r=result))
 
-
-
-
-
-
-
+print(f"my name is {name} {surname} and I am {age} years old.")
 
 
 
